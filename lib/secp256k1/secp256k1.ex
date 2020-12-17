803d6d8a5aa6c4bb8811ad42d45717ec9ddd3323
Philip Glazman <8378656+philipglazman@users.noreply.github.com>
2020-12-17 12:39:00 -0800

    use recid

--- lib/secp256k1/secp256k1.ex
+++ lib/secp256k1/secp256k1.ex
@@ -18,7 +18,7 @@ defmodule Bitcoinex.Secp256k1 do
 
     @spec parse_signature(binary) ::
             {:ok, Signature} | {:error, String.t()}
-    def parse_signature(compact_sig) when byte_size(compact_sig) != 65,
+    def parse_signature(compact_sig) when byte_size(compact_sig) != 64,
       do: {:error, "invalid signature size"}
 
     @doc """
@@ -26,7 +26,7 @@ defmodule Bitcoinex.Secp256k1 do
     """
     def parse_signature(compact_sig) do
       # Get r,s from signature.
-      <<_::binary-size(1), r::binary-size(32), s::binary-size(32), _rest::bytes>> = compact_sig
+      <<r::binary-size(32), s::binary-size(32)>> = compact_sig
 
       r = :binary.decode_unsigned(r)
       s = :binary.decode_unsigned(s)
@@ -49,13 +49,6 @@ defmodule Bitcoinex.Secp256k1 do
           {:ok, %Signature{r: r, s: s}}
       end
     end
-
-    @spec get_iteration(binary) :: integer
-    def get_iteration(compact_sig) do
-      :binary.part(compact_sig, {0, 1})
-      |> :binary.decode_unsigned()
-      |> Kernel.-(27) &&& ~~~4
-    end
   end
 
   @doc """
@@ -64,25 +57,21 @@ defmodule Bitcoinex.Secp256k1 do
   @spec ecdsa_recover_compact(binary, binary, integer) ::
           {:ok, binary} | {:error, String.t()}
   def ecdsa_recover_compact(msg, compact_sig, recoveryId) do
-    # Assign header byte to signature using recoveryID
-    compact_sig = <<recoveryId + 27 + 4>> <> compact_sig
-
     # Parse r and s from the signature.
     case Signature.parse_signature(compact_sig) do
       {:ok, sig} ->
         # Find the iteration.
-        iteration = Signature.get_iteration(compact_sig)
 
         # R(x) = (n * i) + r
         # where n is the order of the curve and R is from the signature.
-        r_x = Params.curve().n * Integer.floor_div(iteration, 2) + sig.r
+        r_x = Params.curve().n * Integer.floor_div(recoveryId, 2) + sig.r
 
         # Check that R(x) is on the curve.
         if r_x > Params.curve().p do
           {:error, "R(x) is not on the curve"}
         else
           # Decompress to get R(y).
-          case get_y(r_x, rem(iteration, 2) == 1) do
+          case get_y(r_x, rem(recoveryId, 2) == 1) do
             {:ok, r_y} ->
               # R(x,y)
               point_r = %Point{x: r_x, y: r_y}
