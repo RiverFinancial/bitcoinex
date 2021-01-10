f3570bd0802f58a103f8b0f6536e42952a9eee75
Sachin Meier <sachin.meier@river.com>
2021-01-09 21:19:50 -0500

    finish pubkey parse

--- lib/secp256k1/point.ex
+++ lib/secp256k1/point.ex
@@ -66,15 +66,34 @@ defmodule Bitcoinex.Secp256k1.Point do
   def sec(%__MODULE__{x: x, y: y}) do
     case rem(y, 2) do
       0 ->
+<<<<<<< HEAD
         <<0x02>> <> Bitcoinex.Utils.pad(:binary.encode_unsigned(x), 32, :leading)
 
       1 ->
         <<0x03>> <> Bitcoinex.Utils.pad(:binary.encode_unsigned(x), 32, :leading)
+=======
+        <<0x02>> <> pad(:binary.encode_unsigned(x))
+      1 ->
+        <<0x03>> <> pad(:binary.encode_unsigned(x))
+>>>>>>> f82d23c (finish pubkey parse)
     end
   end
 
   @doc """
   serialize_public_key serializes a compressed public key to string
+<<<<<<< HEAD
+=======
+  """
+  @spec serialize_public_key(t()) :: String.t()
+  def serialize_public_key(pubkey) do
+    pubkey
+    |> sec()
+    |> Base.encode16(case: :lower)
+  end
+
+  @doc """
+  pads binary to 32 bytes
+>>>>>>> f82d23c (finish pubkey parse)
   """
   @spec serialize_public_key(t()) :: String.t()
   def serialize_public_key(pubkey) do
