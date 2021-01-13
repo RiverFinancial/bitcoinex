39fefd9b07d65cf2cde4c884af0191ebf6c9caeb
Sachin Meier <sachin.meier@river.com>
2021-01-12 19:34:54 -0600

    add wordlists

--- lib/seed.ex
+++ lib/seed.ex
@@ -1,14 +1,25 @@
 defmodule Bitcoinex.Seed do
 	@moduledoc """
-	Contains functionality for BIP32 seeds.
+		Contains functionality for BIP32 seed and extended keys.
   """
+	
+	
+	# @soft_cap 0x80000000
+	# @hard_cap 0x100000000
+	# @pbkdf2_rounds 0x800
 	@strengths [128, 160, 192, 224, 256]
-	@languages [:english, :french, :italian, :chinese_simplified, :chinese_traditional, :japanese, :korean, :spanish]
+	@wordlist_dir "lib/wordlist/"
+
 
 	defstruct [
-		:bits,
-		:strength
+		:bits
 	]
+
+	@type t() :: %__MODULE__{
+		bits: binary
+	}
+
+	@type language :: :english | :french | :italian | :chinese_simplified | :chinese_traditional | :japanese | :korean | :spanish
 
 	def valid_len(bits) do
 		bits_len = byte_size(bits)
@@ -16,11 +27,21 @@ defmodule Bitcoinex.Seed do
 		entropy_len in @strengths
 	end
 
-
 	@spec checksum(binary) :: boolean
-	defp checksum(bits) do
+	def checksum(bits) do
 		byte_size(bits) * 8
 	end
+
+	@spec get_wordlist(language) :: list
+	def get_wordlist(lang) do
+		@wordlist_dir <> to_string(lang)
+		|> Kernel.<>(".txt")
+		|> File.stream!()
+		|> Stream.map(&String.trim/1)
+		|> Enum.to_list()
+	end
+
+	
 
 
 end
