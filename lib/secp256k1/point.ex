6268e610b4566adef25d037ac5e35677f0066e56
Sachin Meier <sachin.meier@river.com>
2021-01-18 15:40:27 -0800

    add comments, parse pubkey from str

--- lib/secp256k1/point.ex
+++ lib/secp256k1/point.ex
@@ -27,19 +27,27 @@ defmodule Bitcoinex.Secp256k1.Point do
   def parse_public_key(<<0x04, x::binary-size(32), y::binary-size(32)>>) do
     %__MODULE__{x: :binary.decode_unsigned(x), y: :binary.decode_unsigned(y)}
   end
-
+  # Above matches with uncompressed keys. Below matches with compressed keys
   def parse_public_key(<<prefix::binary-size(1), x_bytes::binary-size(32)>>) do
     x = :binary.decode_unsigned(x_bytes)
 
-    case rem(:binary.decode_unsigned(prefix), 2) do
-      0 ->
+    case :binary.decode_unsigned(prefix) do
+      2 ->
         {:ok, y} = Bitcoinex.Secp256k1.get_y(x, false)
         %__MODULE__{x: x, y: y}
 
-      1 ->
+      3 ->
         {:ok, y} = Bitcoinex.Secp256k1.get_y(x, true)
         %__MODULE__{x: x, y: y}
     end
+  end
+
+  # Allow parse_public_key to parse SEC strings
+  def parse_public_key(key) do
+    key
+    |> String.downcase()
+    |> Base.decode16!(case: :lower)
+    |> parse_public_key()
   end
 
   @doc """
