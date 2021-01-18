8a873da4a2ad752de98ba0ead3d20fdc6b4ee0f5
Sachin Meier <sachin.meier@river.com>
2021-01-18 15:41:00 -0800

    change key s -> d, comment & update deterministic_k

--- lib/secp256k1/privatekey.ex
+++ lib/secp256k1/privatekey.ex
@@ -6,29 +6,29 @@ defmodule Bitcoinex.Secp256k1.PrivateKey do
   alias Bitcoinex.Base58
 
   @type t :: %__MODULE__{
-          s: integer()
+          d: non_neg_integer()
         }
 
   @enforce_keys [
-    :s
+    :d
   ]
-  defstruct [:s]
+  defstruct [:d]
 
   @doc """
   calculate Point from private key
   """
   @spec serialize_private_key(t()) :: Point.t()
-  def to_point(%__MODULE__{s: s}) do
+  def to_point(%__MODULE__{d: d}) do
     g = %Point{x: Params.curve().g_x, y: Params.curve().g_y, z: 0}
-    Math.multiply(g, s)
+    Math.multiply(g, d)
   end
 
   @doc """
    serialize_private_key serializes a private key into hex
   """
   @spec serialize_private_key(t()) :: String.t()
-  def serialize_private_key(%__MODULE__{s: s}) do
-    s
+  def serialize_private_key(%__MODULE__{d: d}) do
+    d
     |> :binary.encode_unsigned()
     |> Bitcoinex.Utils.pad(32, :leading)
     |> Base.encode16(case: :lower)
@@ -39,8 +39,8 @@ defmodule Bitcoinex.Secp256k1.PrivateKey do
   assumes all keys are compressed
   """
   @spec wif!(t(), Bitcoinex.Network.network_name()) :: String.t()
-  def wif!(%__MODULE__{s: s}, network_name) do
-    :binary.encode_unsigned(s)
+  def wif!(%__MODULE__{d: d}, network_name) do
+    :binary.encode_unsigned(d)
     |> Bitcoinex.Utils.pad(32, :leading)
     |> wif_prefix(network_name)
     |> compressed_suffix()
@@ -76,7 +76,7 @@ defmodule Bitcoinex.Secp256k1.PrivateKey do
       {:error, network_name}
     else
       secret = :binary.decode_unsigned(wif)
-      {:ok, %__MODULE__{s: secret}, network_name, true}
+      {:ok, %__MODULE__{d: secret}, network_name, true}
     end
   end
 
@@ -88,7 +88,7 @@ defmodule Bitcoinex.Secp256k1.PrivateKey do
       {:error, network_name}
     else
       secret = :binary.decode_unsigned(wif)
-      {:ok, %__MODULE__{s: secret}, network_name, false}
+      {:ok, %__MODULE__{d: secret}, network_name, false}
     end
   end
 
@@ -96,40 +96,47 @@ defmodule Bitcoinex.Secp256k1.PrivateKey do
   # encoding
   defp wif_prefix(binary, :mainnet), do: <<0x80>> <> binary
   defp wif_prefix(binary, :testnet), do: <<0xEF>> <> binary
-  # what is the best way to throw an error if an invalid network name is passed?
-  # defp wif_prefix(binary, _), do: {:error, "networks must be in [:mainnet, :testnet]"}
+  defp wif_prefix(binary, :regtest), do: <<0xEF>> <> binary
   # decoding
   defp wif_prefix(<<0x80>>), do: {:ok, :mainnet}
   defp wif_prefix(<<0xEF>>), do: {:ok, :testnet}
   defp wif_prefix(_), do: {:error, "unrecognized network prefix for WIF"}
 
-  @spec deterministic_k(integer, integer) :: integer
-  defp deterministic_k(%__MODULE__{s: s}, raw_z) do
+  def deterministic_k(%__MODULE__{d: d}, raw_z) do
+    # RFC 6979 https://tools.ietf.org/html/rfc6979#section-3.2
     k = :binary.list_to_bin(List.duplicate(<<0x00>>, 32))
     v = :binary.list_to_bin(List.duplicate(<<0x01>>, 32))
     n = Params.curve().n
     z = lower_z(raw_z, n)
+    # 3.2(d) - pad z and privkey for use
     sighash = Bitcoinex.Utils.pad(:binary.encode_unsigned(z), 32, :leading)
-    secret = Bitcoinex.Utils.pad(:binary.encode_unsigned(s), 32, :leading)
+    secret = Bitcoinex.Utils.pad(:binary.encode_unsigned(d), 32, :leading)
+    # 3.2(d) - hmac with key k
     k = :crypto.hmac(:sha256, k, v <> <<0x00>> <> secret <> sighash)
+    # 3.2(e) - update v
     v = :crypto.hmac(:sha256, k, v)
+    # 3.2(f) - update k
     k = :crypto.hmac(:sha256, k, v <> <<0x01>> <> secret <> sighash)
+    # 3.2(g) - update v
     v = :crypto.hmac(:sha256, k, v)
+    # 3.2(h) - algorithm
     final_k = find_candidate(k, v, n)
-    %__MODULE__{s: final_k}
+    %__MODULE__{d: final_k}
   end
 
   defp find_candidate(k, v, n) do
+    # RFC 6979 https://tools.ietf.org/html/rfc6979#section-3.2
     v = :crypto.hmac(:sha256, k, v)
     candidate = :binary.decode_unsigned(v)
-
-    unless candidate >= 1 and candidate < n do
+    # 3.2(h).3 - check candidate in [1,n-1] and r != 0
+    if candidate >= 1 and candidate < n and to_point(%__MODULE__{d: candidate}).x != 0 do
+      candidate
+    else
+      # if candidate is invalid
       k = :crypto.hmac(:sha256, k, v <> <<0x00>>)
       v = :crypto.hmac(:sha256, k, v)
       find_candidate(k, v, n)
     end
-
-    candidate
   end
 
   defp lower_z(z, n) do
@@ -141,8 +148,8 @@ defmodule Bitcoinex.Secp256k1.PrivateKey do
     k = deterministic_k(privkey, z)
     n = Params.curve().n
     sig_r = to_point(k).x
-    inv_k = Math.inv(k.s, n)
-    sig_s = Math.modulo((z + sig_r * privkey.s) * inv_k, n)
+    inv_k = Math.inv(k.d, n)
+    sig_s = Math.modulo((z + sig_r * privkey.d) * inv_k, n)
 
     if sig_s > n / 2 do
       %Signature{r: sig_r, s: n - sig_s}
