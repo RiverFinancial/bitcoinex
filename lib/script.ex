42b76ea38f400c126de8d576d61c3e76b92ac400
Sachin Meier <sachin.meier@river.com>
2021-04-23 17:18:47 -0700

    updates

--- lib/script.ex
+++ lib/script.ex
@@ -1,4 +1,7 @@
 defmodule Bitcoinex.Script do
+	@moduledoc """
+		a module for manipulating Bitcoin Scripts
+	"""
 
 	import Bitcoinex.Opcode
 
@@ -21,9 +24,15 @@ defmodule Bitcoinex.Script do
 	@spec new() :: t()
 	def new, do: %__MODULE__{s: []}
 
-	@spec empty(t()) :: bool
-	def empty(%__MODULE__{s: []}), do: true
-	def empty(_), do: false
+	@spec is_true?(t()) :: bool
+	def is_true?(%__MODULE__{s: [0x51]}), do: true
+	def is_true?(%__MODULE__{s: [:op_true]}), do: true
+	def is_true?(_), do: false
+
+
+	@spec empty?(t()) :: bool
+	def empty?(%__MODULE__{s: []}), do: true
+	def empty?(_), do: false
 
 	@spec get_op_num(atom) :: non_neg_integer()
 	def get_op_num(op), do: Map.fetch(opcode_atoms(), op)
@@ -38,6 +47,7 @@ defmodule Bitcoinex.Script do
 	@spec push_op(atom | non_neg_integer(), t()) :: t()
 
 	def push_op(%__MODULE__{s: stack}, item) do
+		# item is opcode num
 		if is_integer(item) and item > 0 and item < 0xff do
 			%__MODULE__{s: [item | stack]}
 		else
@@ -56,23 +66,15 @@ defmodule Bitcoinex.Script do
 	@spec push_data(binary, t()) :: t()
 	def push_data(script, data) do
 		datalen = byte_size(data)
-		# little_datalen = :binary.encode_unsigned(datalen, :little)
 		script = push_raw_data(script, data)
 		cond do
 			datalen < 0x4c ->
-				# %__MODULE__{s: [datalen | data | stack]}
 				push_op(script, datalen)
 			datalen <= 0xff ->
-				#%__MODULE__{s: [datalen | data | stack]} |> push_op(:op_pushdata1)
-				# push_raw_data(script, datalen)
 				push_op(script, :op_pushdata1)
 			datalen <= 0xffff ->
-				# %__MODULE__{s: [datalen | data | stack]} |> push_op(:op_pushdata2)
-				# push_raw_data(script, datalen)
 				push_op(script, :op_pushdata2)
 			datalen <= 0xffffffff ->
-				# %__MODULE__{s: [datalen | data | stack]} |> push_op(:op_pushdata4)
-				# push_raw_data(script, datalen)
 				push_op(script, :op_pushdata4)
 			true ->
 				{:error, "invalid data length, must be 0..0xffffffff, got #{datalen}"}
@@ -92,11 +94,10 @@ defmodule Bitcoinex.Script do
 				len = Utils.int_to_little(len, 1)
 				serializer(%__MODULE__{s: script}, acc <> len <> item)
 			len <= 0xff -> 
-				# n = Utils.int_to_little(76, 1)
 				len = Utils.int_to_little(len, 1)
 				serializer(%__MODULE__{s: script}, acc <> len <> item)
-			len <= 0x0208 -> 
-				# n = Utils.int_to_little(77, 1)
+			# PUSHDATA4 limited to 520 bytes
+			len <= 0x0208->
 				len = Utils.int_to_little(len, 2)
 				serializer(%__MODULE__{s: script}, acc <> len <> item)
 			true -> {:error, "data is too long"}
@@ -109,7 +110,11 @@ defmodule Bitcoinex.Script do
 		Utils.encode_int(len) <> script
 	end
 
-	def to_hex(script), do: serialize(script) |> Base.encode16(case: :lower)
+	def to_hex(script) do
+		 script
+		 |> serialize() 
+		 |> Base.encode16(case: :lower)
+	end 
 
 	def raw_combine(%__MODULE__{s: s1}, %__MODULE__{s: s2}), do: %__MODULE__{s: s1 ++ s2}
 	
@@ -131,8 +136,8 @@ defmodule Bitcoinex.Script do
 	defp display_script(%__MODULE__{s: [item | stack]}, acc) when is_binary(item) do
 		display_script(%__MODULE__{s: stack}, acc <> " " <> Base.encode16(item, case: :lower) ) 
 	end
+
 	#TODO to_address from address
-	#TODO construct scripts given hashes
 	#TODO parse scripts
 
 	# SCRIPT TYPE DETERMINERS
@@ -197,6 +202,40 @@ defmodule Bitcoinex.Script do
 		end
 	end
 
+	# CREATE COMMON SCRIPTS 
 
+	def create_p2pkh(<<pkh::binary-size(20)>>) do
+		new()
+		|> push_op(0xac)
+		|> push_op(0x88)
+		|> push_data(pkh)
+		|> push_op(0xa9)
+		|> push_op(0x76)
+	end
+	def create_p2pkh(_), do: {:error, "pubkey hash must be a 20-byte hash"}
 
+	def create_p2sh(<<sh::binary-size(20)>>) do
+		new()
+		|> push_op(0x87)
+		|> push_data(sh)
+		|> push_op(0xa9)
+	end
+	def create_p2sh(_), do: {:error, "script hash must be a 20-byte hash"}
+
+	def create_witness_script(witver, witness_program) do
+		new()
+		|> push_data(witness_program)
+		|> push_op(witver)
+	end
+
+	def create_p2wpkh(<<pkh::binary-size(20)>>) do: create_witness_script(0x00, pkh)
+	def create_p2wpkh(_), do: {:error, "pubkey hash must be a 20-byte hash"}
+
+	def create_p2wsh(<<sh::binary-size(32)>>) do: create_witness_script(0x00, sh)
+	def create_p2wsh(_), do: {:error, "script hash must be a 32-byte hash"}
+
+	def create_p2tr(<<pk::binary-size(32)>>), do: create_witness_script(0x01, pk)
+	def create_p2tr(_), do: {:error, "public key must be 32-bytes"}
+
+	
 end
