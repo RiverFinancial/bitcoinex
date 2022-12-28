815438d39380e843eff19e40a5e9aae6bff07255
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2022-12-28 10:30:59 -0700

    Update mix_test_watch

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Bitcoinex.MixProject do
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.2.0", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
-      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.1", only: :dev, runtime: false},
       {:stream_data, "~> 0.1", only: :test},
       {:timex, "~> 3.1"},
       {:decimal, "~> 1.0 or ~> 2.0"},
