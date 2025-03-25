b8f3099275bccc46ea98b7f9ce7bef20718e776a
asalomeri <ahmdalmyry82@gmail.com>
2025-03-25 15:16:01 +0300

    Update mix.exs
    defmodule Bitcoinex.MixProject do
      use Mix.Project
    
      def project do
        [
          app: :bitcoinex,
          version: "0.1.8",
          elixir: "~> 1.11",
          package: package(),
          start_permanent: Mix.env() == :prod,
          dialyzer: dialyzer(),
          deps: deps(),
          aliases: aliases(),
          description: description(),
          source_url: "https://github.com/RiverFinancial/bitcoinex"
        ]
      end
    
      def application do
        [
          extra_applications: [:logger]
        ]
      end
    
      defp deps do
        [
          {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
          {:dialyxir, "~> 1.2.0", only: [:dev, :test], runtime: false},
          {:excoveralls, "~> 0.10", only: :test},
          {:mix_test_watch, "~> 1.1", only: :dev, runtime: false},
          {:stream_data, "~> 0.1", only: :test},
          {:decimal, "~> 1.0 or ~> 2.0"},
          {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
          {:benchee, ">= 1.0.0", only: :dev}
        ]
      end
    
      defp aliases do
        [
          "lint.all": [
            "format --check-formatted",
            "credo --strict --only warning",
            "dialyzer --halt-exit-status"
          ],
          compile: ["compile --warnings-as-errors"]
        ]
      end
    
      defp dialyzer do
        [
          plt_file: plt_file(),
          flags: [
            :error_handling
          ],
          ignore_warnings: ".dialyzer_ignore.exs"
        ]
      end
    
      defp plt_file do
        {:no_warn, "_plts/dialyzer.plt"}
      end
    
      defp package do
        [
          files: ~w(lib test .formatter.exs mix.exs README.md UNLICENSE CHANGELOG.md SECURITY.md),
          licenses: ["Unlicense"],
          links: %{"GitHub" => "https://github.com/RiverFinancial/bitcoinex"}
        ]
      end
    
      defp description() do
        "Bitcoinex is a Bitcoin Library for Elixir."
      end
    end

--- mix.exs
+++ mix.exs
@@ -76,3 +76,77 @@ defmodule Bitcoinex.MixProject do
     "Bitcoinex is a Bitcoin Library for Elixir."
   end
 end
+defmodule Bitcoinex.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bitcoinex,
+      version: "0.1.8",
+      elixir: "~> 1.11",
+      package: package(),
+      start_permanent: Mix.env() == :prod,
+      dialyzer: dialyzer(),
+      deps: deps(),
+      aliases: aliases(),
+      description: description(),
+      source_url: "https://github.com/RiverFinancial/bitcoinex"
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:mix_test_watch, "~> 1.1", only: :dev, runtime: false},
+      {:stream_data, "~> 0.1", only: :test},
+      {:decimal, "~> 1.0 or ~> 2.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:benchee, ">= 1.0.0", only: :dev}
+    ]
+  end
+
+  defp aliases do
+    [
+      "lint.all": [
+        "format --check-formatted",
+        "credo --strict --only warning",
+        "dialyzer --halt-exit-status"
+      ],
+      compile: ["compile --warnings-as-errors"]
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_file: plt_file(),
+      flags: [
+        :error_handling
+      ],
+      ignore_warnings: ".dialyzer_ignore.exs"
+    ]
+  end
+
+  defp plt_file do
+    {:no_warn, "_plts/dialyzer.plt"}
+  end
+
+  defp package do
+    [
+      files: ~w(lib test .formatter.exs mix.exs README.md UNLICENSE CHANGELOG.md SECURITY.md),
+      licenses: ["Unlicense"],
+      links: %{"GitHub" => "https://github.com/RiverFinancial/bitcoinex"}
+    ]
+  end
+
+  defp description() do
+    "Bitcoinex is a Bitcoin Library for Elixir."
+  end
+end
